1d8c7f7af8a5217dbd309a296f08886214b02b6a
Nuccia955 <elenamunaro1995@gmail.com>
2021-11-19 14:28:48 +0100

    Includes Axios script and initialize it

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 const app = new Vue ({
     el: '#app',
     data: {
+        APIurl: 'https://flynn.boolean.careers/exercises/api/random/mail',
         email: [
             "test",
         ]
     },
     created() {
-
+        this.genEmailList()
     },
     methods: {
-
+        genEmailList() {
+            axios.get(this.APIurl)
+                .then(function (response) {
+                    // handle success
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                }
+            )
+        }
     },
 })
